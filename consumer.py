85d95d39a4111587b4ee1251a9492bc6b8184cd4
Andrei Chirac <56991561+AndreiChirac@users.noreply.github.com>
2022-04-06 17:58:40 +0300

    Update consumer.py

--- consumer.py
+++ consumer.py
@@ -37,27 +37,26 @@ class Consumer(Thread):
         self.marketplace = marketplace
         self.retry_wait_time = retry_wait_time
 
-    """
-    metoda care realizeaza cererile de adaugare a unui produs in cos
-    """
     def add_request(self, requests, new_cart_id):
+        """
+        metoda care realizeaza cererile de adaugare a unui produs in cos
+        """
         requests_made = 1
         while True:
 
             if requests_made > requests["quantity"]:
                 break
 
-            error_code = self.marketplace.add_to_cart(
-                new_cart_id, requests["product"])
-
-            if error_code or error_code is None:
+            if self.marketplace.add_to_cart(
+                    new_cart_id, requests["product"]):
                 requests_made += 1
             else:
                 time.sleep(self.retry_wait_time)
-    """
-    metoda care realizeaza cererile de scoatere a unui produs din cos
-    """
+
     def rm_request(self, requests, new_cart_id):
+        """
+        metoda care realizeaza cererile de scoatere a unui produs din cos
+        """
         requests_made = 1
         while True:
 
@@ -67,30 +66,28 @@ class Consumer(Thread):
             error_code = self.marketplace.remove_from_cart(
                 new_cart_id, requests["product"])
 
-            if error_code or error_code is None:
+            if error_code is None:
                 requests_made += 1
             else:
                 time.sleep(self.retry_wait_time)
 
     def run(self):
         """
-        luam fiecare lista din cadrul lui carts deoarece pentru fiecare lista vom avea un id nou pentru cos
+        luam fiecare lista din cadrul lui carts deoarece pentru fiecare lista vom avea un id nou
+        pentru cos
         """
         for new_cart in self.carts:
 
             new_cart_id = self.marketplace.new_cart()
-            """
-            parcurgem fiecare operatie de add sau remove si o tratam specific 
-            """
+
+            # parcurgem fiecare operatie de add sau remove si o tratam specific
             for requests in new_cart:
                 if requests["type"] == "add":
                     self.add_request(requests, new_cart_id)
                 else:
                     self.rm_request(requests, new_cart_id)
-            """
-            dupa ce am terminat toate opertaiile din cadrul listei trebuie sa finalizam comanda
-            si sa printam pentru a genera output-ul
-            """
+
+            # dupa ce am terminat toate opertaiile din cadrul listei trebuie sa finalizam comanda
+            # si sa printam pentru a genera output-ul
             for product in self.marketplace.place_order(new_cart_id):
                 print(self.name + " bought " + str(product))
-
